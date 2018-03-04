1647e185438ce50255a05fdafe782783e33bb5bd
adamrtalbot <talbot.a.r@gmail.com>
2018-03-04 16:50:08 +0000

    Added tximport script

---
+++ tximport_kallisto.R
@@ -0,0 +1,43 @@
+output_folder = commandArgs(trailingOnly=TRUE)
+
+## Check for presence of tximport, if so load, if not install and load
+
+if(!require("tximport")){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("tximport")
+}
+
+library(tximport)
+
+load_kallisto <- function(output_folder){
+  
+  # Gather list of abundance files
+  kallisto_output_files <- file.path(list.files(path = output_folder, 
+                                                pattern = "abundance.h5", 
+                                                recursive = TRUE, 
+                                                full.names = TRUE))
+  
+  
+  # Set names of kallisto_output_files
+  
+  names(kallisto_output_files) <- unlist(lapply(strsplit(x = kallisto_output_files, split = '/'), function(x) x[length(x) - 1]))
+  
+  # Extract count data from kallisto output folders
+  
+  kallist_counts <- tximport(files = kallisto_output_files, 
+                             type = "kallisto", 
+                             txIn = TRUE, 
+                             txOut = TRUE)
+  
+  # Return just pseudocount data
+  
+  return(kallist_counts$counts)
+}
+
+write.csv(
+  x = load_kallisto(output_folder = output_folder),
+  file = paste(output_folder, "count_data", sep = "/")
+)
+
+
+
